621aaa8ea398911f9a1e0f46ae356cc779ec5e33
Lee Jincheon <pressky99@gmail.com>
2023-09-06 20:49:45 +0900

    [level 2] Title: 구명보트, Time: 8.72 ms, Memory: 53.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        
+        Arrays.sort(people);
+        
+        int lt = 0;
+        int rt = people.length-1;
+        
+        while(lt<=rt){
+            if(lt == rt){ // 마지막 1명 남았을 때
+                if(people[lt] <= limit){
+                    answer++;
+                    break;
+                }
+            }
+            if(people[lt]+people[rt] <= limit){ // 2명을 태울 수 있으면 태움
+                answer++;
+                lt++;
+                rt--;
+            }else{ // 2명을 태울 수 없을 때
+                if(people[rt] <= limit){ // 제일 무거운 사람을 태움
+                    answer++;
+                    rt--;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}

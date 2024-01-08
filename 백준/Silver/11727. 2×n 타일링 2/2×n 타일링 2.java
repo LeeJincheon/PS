cc19752a576d9c23511ac4c41e36e430a5c0110f
Jin <pressky99@gmail.com>
2024-01-08 16:59:21 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 124 ms, Memory: 14344 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -9,7 +9,7 @@ public class Main {
         dp[1] = 1;
         dp[2] = 3;
 
-        for (int i=3; i<dp.length; i++) {
+        for (int i=3; i<=n; i++) {
             dp[i] = (dp[i-1] + dp[i-2]*2)%10007;
         }
 

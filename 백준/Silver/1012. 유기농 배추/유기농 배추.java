b75b50fff43063e3573c4fd23feaa47fe7ce16ed
Jin <pressky99@gmail.com>
2024-01-11 20:37:32 +0900

    [Silver II] Title: 유기농 배추, Time: 176 ms, Memory: 16000 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,61 +1,69 @@
+import java.io.*;
 import java.util.*;
+
 public class Main {
-	
-	static int m, n;
-	static int[][] map, visited;
-	static int[] dx = {0, 1, 0, -1};
-	static int[] dy = {-1, 0, 1, 0};
-	
-	static void bfs(int x, int y) {
-		Queue<Integer> q = new LinkedList<>();
-		q.add(x);
-		q.add(y);
-		visited[y][x] = 1;
-		
-		while(!q.isEmpty()) {
-			int nowX = q.poll();
-			int nowY = q.poll();
-			for(int i=0; i<4; i++) {
-				int newX = nowX + dx[i];
-				int newY = nowY + dy[i];
-				if(newX>=0 && newX<m && newY>=0 && newY<n) {
-					if(map[newY][newX]==1 && visited[newY][newX]==0) {
-						q.add(newX);
-						q.add(newY);
-						visited[newY][newX] = 1;
-					}
-				}
-			}
-		}
-	}
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int tc = sc.nextInt();
-		for(int i=0; i<tc; i++) {
-			m = sc.nextInt();
-			n = sc.nextInt();
-			int k = sc.nextInt();
-			map = new int[n][m];
-			visited = new int[n][m];
-			
-			for(int j=0; j<k; j++) {
-				int x = sc.nextInt();
-				int y = sc.nextInt();
-				map[y][x] = 1;
-			}
-			
-			int cnt= 0;
-			for(int j=0; j<n; j++) {
-				for(int t=0; t<m; t++) {
-					if(map[j][t]==1 && visited[j][t]==0) {
-						bfs(t, j);
-						cnt++;
-					}
-				}
-			}
-			System.out.println(cnt);
-		}
-	}
+
+    static int n, m;
+    static int[][] map;
+    static boolean[][] visited;
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {-1, 0, 1, 0};
+
+    static void bfs(int x, int y) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(x);
+        q.add(y);
+        visited[y][x] = true;
+
+        while(!q.isEmpty()) {
+            int nowX = q.poll();
+            int nowY = q.poll();
+            for (int i=0; i<4; i++) {
+                int newX = nowX + dx[i];
+                int newY = nowY + dy[i];
+                if (newX >= 0 && newX < m && newY >= 0 && newY < n && map[newY][newX] == 1 && !visited[newY][newX]) {
+                    q.add(newX);
+                    q.add(newY);
+                    visited[newY][newX] = true;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int t = Integer.parseInt(br.readLine());
+
+        for (int i=0; i<t; i++) {
+            st = new StringTokenizer(br.readLine());
+            m = Integer.parseInt(st.nextToken());
+            n = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+
+            map = new int[n][m];
+            visited = new boolean[n][m];
+
+            for (int j=0; j<k; j++) {
+                st = new StringTokenizer(br.readLine());
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
+                map[y][x] = 1;
+            }
+
+            int cnt = 0;
+            for (int p=0; p<n; p++) {
+                for (int q=0; q<m; q++) {
+                    if (map[p][q] == 1 && !visited[p][q]) {
+                        bfs(q, p);
+                        cnt++;
+                    }
+                }
+            }
+
+            System.out.println(cnt);
+        }
+    } // main
+
 }

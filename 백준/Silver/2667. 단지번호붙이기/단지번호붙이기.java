e349ccc5f932f8da8d9fa3bc9654008046c1a4d5
Jin <pressky99@gmail.com>
2024-01-11 17:52:28 +0900

    [Silver I] Title: 단지번호붙이기, Time: 140 ms, Memory: 14364 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,65 +1,63 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-	
-	static int n;
-	static int[] dx = {0, 1, 0, -1};
-	static int[] dy = {-1, 0, 1, 0};
-	static int[][] visited;
-	static ArrayList<Integer> area = new ArrayList<>();
-	
-	static void bfs(int x, int y, String[] map) {
-		Queue<Integer> q = new LinkedList<>();
-		
-		q.add(x);
-		q.add(y);
-		visited[y][x] = 1;
-		int sum = 1;
-		
-		while(!q.isEmpty()) {
-			int nowX = q.poll();
-			int nowY = q.poll();
-			for(int i=0; i<4; i++) {
-				int newX = nowX + dx[i];
-				int newY = nowY + dy[i];
-				if(newX>=0 && newX<n && newY>=0 && newY<n && visited[newY][newX]==0) {
-					if(map[newY].charAt(newX)-'0' == 1) {
-						q.add(newX);
-						q.add(newY);
-						visited[newY][newX] = 1;
-						sum++;
-					}
-				}
-			}
-		}
-		area.add(sum);
-	}
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		n = sc.nextInt();
-		visited = new int[n][n];
-		String[] map = new String[n];
-		
-		for(int i=0; i<n; i++) {
-			map[i] = sc.next();
-		}
-		
-		int cnt = 0;
-		for(int i=0; i<n; i++) {
-			for(int j=0; j<n; j++) {
-				if(map[i].charAt(j)-'0'==1 && visited[i][j]==0) {
-					bfs(j, i, map);
-					cnt++;
-				}
-			}
-		}
-		System.out.println(cnt);
-		
-		Collections.sort(area);
-		for(int i=0; i<cnt; i++) {
-			System.out.println(area.get(i));
-		}
-	}
+
+    static int n;
+    static String[] map;
+    static boolean[][] visited;
+    static List<Integer> houseCount = new ArrayList<>();
+    static int complexCount = 0;
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {-1, 0, 1, 0};
+
+    static void bfs(int x, int y) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(x);
+        q.add(y);
+        visited[y][x] = true;
+        int cnt = 1;
+
+        while(!q.isEmpty()) {
+            int nowX = q.poll();
+            int nowY = q.poll();
+            for (int i=0; i<4; i++) {
+                int newX = nowX + dx[i];
+                int newY = nowY + dy[i];
+                if (newX >= 0 && newX < n && newY >= 0 && newY < n && map[newY].charAt(newX) == '1' && !visited[newY][newX]) {
+                    q.add(newX);
+                    q.add(newY);
+                    visited[newY][newX] = true;
+                    cnt++;
+                }
+            }
+        }
+        houseCount.add(cnt);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+
+        map = new String[n];
+        visited = new boolean[n][n];
+
+        for (int i=0; i<n; i++) {
+            map[i] = br.readLine();
+        }
+
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) {
+                if (map[i].charAt(j) == '1' && !visited[i][j]) {
+                    bfs(j, i);
+                    complexCount++;
+                }
+            }
+        }
+
+        System.out.println(complexCount);
+        houseCount.stream().sorted().forEach(System.out::println);
+    } // main
+
 }

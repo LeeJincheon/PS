0cdaf17f4e6a56b0c71edcd9fc48242beae1217c
Jin <pressky99@gmail.com>
2024-01-12 16:54:00 +0900

    [Silver I] Title: 나이트의 이동, Time: 392 ms, Memory: 96416 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
@@ -1,61 +1,64 @@
+import java.io.*;
 import java.util.*;
+
 public class Main {
-	
-	static int n;
-	static int[][] map;
-	static int[][] visited;
-	static int[] dx = {1, 2, 2, 1, -1, -2, -2, -1};
-	static int[] dy = {-2, -1, 1, 2, 2, 1, -1, -2};
-	
-	static void bfs(int x, int y, int endX, int endY) {
-		Queue<Integer> q = new LinkedList<>();
-		q.add(x);
-		q.add(y);
-		
-		while(!q.isEmpty()) {
-			int nowX = q.poll();
-			int nowY = q.poll();
-			for(int i=0; i<8; i++) {
-				int newX = nowX + dx[i];
-				int newY = nowY + dy[i];
-				
-				if(newX==endX && newY==endY) {
-					visited[newY][newX] = visited[nowY][nowX] + 1;
-					return;
-				}
-				
-				if(newX>=0 && newX<n && newY>=0 && newY<n) {
-					if(visited[newY][newX]==0) {
-						q.add(newX);
-						q.add(newY);
-						visited[newY][newX] = visited[nowY][nowX] + 1;
-					}
-				}
-			}
-		}
-	}
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int tc = sc.nextInt();
-		
-		for(int i=0; i<tc; i++) {
-			n = sc.nextInt();
-			int startX = sc.nextInt();
-			int startY = sc.nextInt();
-			int endX = sc.nextInt();
-			int endY = sc.nextInt();
-			
-			visited = new int[n][n];
-			
-			bfs(startX, startY, endX, endY);
-			
-			if(startX==endX && startY==endY) {
-				System.out.println(0);
-			}else {
-				System.out.println(visited[endY][endX]);
-			}
-		}
-	}
+
+    static int size, x, y, tx, ty;
+    static int[][] map;
+    static int[][] visited;
+    static int[] dx = {1, 2, 2, 1, -1, -2, -2, -1};
+    static int[] dy = {-2, -1, 1, 2, 2, 1, -1, -2};
+
+    static void bfs() {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(x);
+        q.add(y);
+        visited[y][x] = 1;
+
+        while(!q.isEmpty()) {
+            int nowX = q.poll();
+            int nowY = q.poll();
+
+            if (nowX == tx && nowY == ty) {
+                return;
+            }
+
+            for (int i=0; i<dx.length; i++) {
+                int newX = nowX + dx[i];
+                int newY = nowY + dy[i];
+                if (newX >= 0 && newX < size && newY >= 0 && newY < size && visited[newY][newX] == 0) {
+                    q.add(newX);
+                    q.add(newY);
+                    visited[newY][newX] = visited[nowY][nowX] + 1;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int testCase = Integer.parseInt(br.readLine());
+
+        for (int i=0; i<testCase; i++) {
+            size = Integer.parseInt(br.readLine());
+            
+            map = new int[size][size];
+            visited = new int[size][size];
+            
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            x = Integer.parseInt(st.nextToken());
+            y = Integer.parseInt(st.nextToken());
+            
+            st = new StringTokenizer(br.readLine());
+            tx = Integer.parseInt(st.nextToken());
+            ty = Integer.parseInt(st.nextToken());
+            
+            bfs();
+
+            System.out.println(visited[ty][tx]-1);
+        }
+
+    } // main
+
 }

7f7b318697dd7a8e901ab6087e403bbc4d140144
Jin <pressky99@gmail.com>
2024-01-17 19:57:23 +0900

    [Silver I] Title: 스타트링크, Time: 224 ms, Memory: 58004 KB -BaekjoonHub

--- 백준/Silver/5014. 스타트링크/스타트링크.java
+++ 백준/Silver/5014. 스타트링크/스타트링크.java
@@ -1,48 +1,53 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-	
-	static int F, S, G, U, D;
-	static int[] cnt, dx;
-	static boolean[] visited;
-	
-	static void bfs(int pos) {
-		Queue<Integer> q = new LinkedList<>();
-		
-		q.add(pos);
-		visited[pos] = true;
-		
-		while(!q.isEmpty()) {
-			int now = q.poll();
-			if(now == G) {
-				System.out.println(cnt[now]);
-				return;
-			}
-            for(int i=0; i<2; i++) {
-				int next = now + dx[i];
-				if(next<=F && next>=1 && visited[next]==false) {
-					q.add(next);
-						visited[next] = true;
-					cnt[next] = cnt[now] + 1;
-				}
-			}
-		}
-		System.out.println("use the stairs");
-	}
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		F = sc.nextInt();
-		S = sc.nextInt();
-		G = sc.nextInt();
-		U = sc.nextInt();
-		D = sc.nextInt();
-		
-		cnt = new int[F+1];
-		visited = new boolean[F+1];
-		dx = new int[]{U, -D};
-		
-		bfs(S);
-	}
+
+    static int F, S, G, U, D;
+    static int[] visited;
+
+    static void bfs(int start) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(start);
+        visited[start] = 0;
+
+        while (!q.isEmpty()) {
+            int now = q.poll();
+            int next = 0;
+            for (int i=0; i<2; i++) {
+                if (i == 0) {
+                    next = now + U;
+                } else {
+                    next = now - D;
+                }
+                if (next > 0 && next <= F && visited[next] == -1) {
+                    q.add(next);
+                    visited[next] = visited[now] + 1;
+                }
+
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] inputs = br.readLine().split(" ");
+        F = Integer.parseInt(inputs[0]);
+        S = Integer.parseInt(inputs[1]);
+        G = Integer.parseInt(inputs[2]);
+        U = Integer.parseInt(inputs[3]);
+        D = Integer.parseInt(inputs[4]);
+
+        visited = new int[1000001];
+        Arrays.fill(visited, -1);
+
+        bfs(S);
+
+        if (visited[G] == -1) {
+            System.out.println("use the stairs");
+        } else {
+            System.out.println(visited[G]);
+        }
+    } // main
 }

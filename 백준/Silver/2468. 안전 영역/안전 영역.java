b26cf6bfeb5d49f69ebb96d8e884c191cb8c0193
Jin <pressky99@gmail.com>
2024-01-24 00:02:23 +0900

    [Silver I] Title: 안전 영역, Time: 256 ms, Memory: 18584 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -1,55 +1,56 @@
-import java.util.*;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
-	
-	static int n, h;
-	static int[][] map;
-	static boolean[][] visited;
-	static int[] dx = {0, 1, 0, -1};
-	static int[] dy = {-1, 0, 1, 0};
-	
-	static void dfs(int x, int y) {
-		visited[y][x] = true;
-		for(int i=0; i<4; i++) {
-			int nx = x + dx[i];
-			int ny = y + dy[i];
-			if(nx>=0 && nx<n && ny>=0 && ny<n) {
-				if(visited[ny][nx]==false && map[ny][nx]>h) {
-					dfs(nx, ny);
-				}
-			}
-		}
-	}
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		n = sc.nextInt();
-		map = new int[n][n];
-		
-		for(int i=0; i<n; i++) {
-			for(int j=0; j<n; j++) {
-				map[i][j] = sc.nextInt();
-			}
-		}
-		int cnt = 0;
-		int res = 0;
-		for(h=0; h<100; h++) {
-			visited = new boolean[n][n];
-			cnt = 0;
-			for(int i=0; i<n; i++) {
-				for(int j=0; j<n; j++) {
-					if(map[i][j]>h && visited[i][j]==false) {
-						dfs(j, i);
-						cnt++;
-					}
-				}
-			}
-			res = Math.max(res, cnt);
-			if(cnt == 0) {
-				break;
-			}
-		}
-		System.out.println(res);
-	}
+
+    static int n, tmp;
+    static int cnt = 1;
+    static int max = Integer.MIN_VALUE;
+    static int[][] map;
+    static boolean[][] visited;
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {-1, 0, 1, 0};
+
+    static void dfs(int x, int y, int w) {
+        visited[y][x] = true;
+
+        for (int i=0; i<4; i++) {
+            int nextX = x + dx[i];
+            int nextY = y + dy[i];
+            if (nextX >= 0 && nextX < n && nextY >= 0 && nextY < n) {
+                if (map[nextY][nextX] > w && !visited[nextY][nextX]) {
+                    dfs(nextX, nextY, w);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        map = new int[n][n];
+        for (int i=0; i<n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j=0; j<n; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+                max = Math.max(max, map[i][j]);
+            }
+        }
+
+        for (int i=1; i<=max; i++) {
+            tmp = 0;
+            visited = new boolean[n][n];
+            for (int j=0; j<n; j++) {
+                for (int k=0; k<n; k++) {
+                    if (map[j][k] > i && !visited[j][k]) {
+                        dfs(k, j, i);
+                        tmp++;
+                    }
+                }
+            }
+            cnt = Math.max(cnt, tmp);
+        }
+
+        System.out.println(cnt);
+    } // main
 }

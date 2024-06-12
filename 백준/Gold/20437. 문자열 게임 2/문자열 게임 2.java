45ab5796bf41a95dca21701cdb7a828ec7c388d8
Jin <pressky99@gmail.com>
2024-06-13 02:05:19 +0900

    [Gold V] Title: 문자열 게임 2, Time: 472 ms, Memory: 42312 KB -BaekjoonHub

--- 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.java
+++ 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.java
@@ -2,16 +2,18 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.valueOf(br.readLine());
+        
+        int t = Integer.parseInt(br.readLine());
+        StringBuilder result = new StringBuilder();
         
         for (int i = 0; i < t; i++) {
             String w = br.readLine();
-            int k = Integer.valueOf(br.readLine());
+            int k = Integer.parseInt(br.readLine());
             
             if (k == 1) {
-                System.out.println(1 + " " + 1);
+                result.append("1 1\n");
                 continue;
             }
             
@@ -36,10 +38,12 @@ public class Main {
             }
             
             if (found) {
-                System.out.println(minLength + " " + maxLength);
+                result.append(minLength).append(" ").append(maxLength).append("\n");
             } else {
-                System.out.println(-1);
+                result.append(-1).append("\n");
             }
         }
+        
+        System.out.print(result.toString());
     }
 }

1c739962ce81b44bb3baa3f248c832b92b8bf03b
Jin <pressky99@gmail.com>
2024-09-18 01:18:58 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/README.md
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/README.md
@@ -1,6 +1,6 @@
 # [level 3] 오랜 기간 보호한 동물(1) - 59044 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/59044) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/59044#qna) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2024년 09월 18일 01:10:21
+2024년 09월 18일 01:18:58
 
 ### 문제 설명
 
--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,9 +1,17 @@
 -- 코드를 입력하세요
-select name, datetime
-from animal_ins
-where animal_id not in(
-    select animal_id
-    from animal_outs
-)
+# select name, datetime
+# from animal_ins
+# where animal_id not in(
+#     select animal_id
+#     from animal_outs
+# )
+# order by datetime
+# limit 3;
+
+select ins.name, ins.datetime
+from animal_ins as ins
+    left join animal_outs as outs
+    on ins.animal_id = outs.animal_id
+where outs.datetime is null
 order by datetime
 limit 3;

d3cc6af2d8d57d6d9aefd36b78bf71eae782442c
Jin <pressky99@gmail.com>
2024-09-18 02:41:53 +0900

    [level 2] Title: 가장 큰 수, Time: 173.56 ms, Memory: 141 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,23 +1,20 @@
-import java.util.Arrays;
+import java.util.*;
+import java.util.stream.*;
 
 class Solution {
     public String solution(int[] numbers) {
-        String[] strNumbers = Arrays.stream(numbers)
+        List<String> strNumbers = Arrays.stream(numbers)
                                     .mapToObj(String::valueOf)
-                                    .toArray(String[]::new);
+                                    .collect(Collectors.toList());
         
-        Arrays.sort(strNumbers, (num1, num2) -> {
-            String order1 = num1 + num2;  // num1이 앞에 올 때
-            String order2 = num2 + num1;  // num2가 앞에 올 때
-            return order2.compareTo(order1); // 더 큰 값이 앞으로 오도록 내림차순 정렬
-        });
+        Collections.sort(strNumbers, (n1, n2) -> (n2+n1).compareTo(n1+n2));
         
         // 정렬된 결과가 "0"으로 시작하면 "0"을 반환 (모든 값이 0일 때)
-        if (strNumbers[0].equals("0")) {
+        if (strNumbers.get(0).equals("0")) {
             return "0";
         }
         
         // 문자열을 이어 붙여서 반환
-        return String.join("", strNumbers);
+        return strNumbers.stream().collect(Collectors.joining());
     }
 }
